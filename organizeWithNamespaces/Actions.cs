811eb4fe75b7e88c2cd0e8a885fbdc5184e6ba34
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-03-11 23:56:04 -0600

    Initial commit

---
+++ organizeWithNamespaces/Actions.cs
@@ -0,0 +1,15 @@
+namespace App.Actions
+{
+    public class UserActions
+    {
+        public static void ViewAccount()
+        {
+            Console.WriteLine("Account details displayed.");
+        }
+
+        public static void UpdateAccount()
+        {
+            Console.WriteLine("Account has been updated.");
+        }
+    }
+}
---
+++ organizeWithNamespaces/Menu.cs
@@ -0,0 +1,2 @@
+using App.cs;
+Console.WriteLine("Hello, World!");
